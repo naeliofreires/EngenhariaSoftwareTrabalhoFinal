a4c0257e40445bc427148240a909bfdf36dbf612
naeliofreires <naeliosistemas@gmail.com>
2017-05-31 16:57:46 -0300

    Spring Configurado

---
+++ src/br/com/gimovel/controller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.gimovel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	String home(){
+		return "index";
+	}
+}
