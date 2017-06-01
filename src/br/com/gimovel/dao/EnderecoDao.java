4c4c11f368319ac841a3df8097dd852a602329f4
naeliofreires <naeliosistemas@gmail.com>
2017-06-01 10:01:23 -0300

    preparando classes Dao Imovel/Endereco

---
+++ src/br/com/gimovel/dao/EnderecoDao.java
@@ -0,0 +1,24 @@
+package br.com.gimovel.dao;
+
+import java.util.ArrayList;
+
+import br.com.gimovel.model.Endereco;
+
+public class EnderecoDao {
+
+	public void adicionar(){
+		
+	}
+	
+	public void remover(){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public ArrayList<Endereco> getEnderecos(){
+		return null;
+	}
+}
---
+++ src/br/com/gimovel/dao/ImovelDao.java
@@ -0,0 +1,25 @@
+package br.com.gimovel.dao;
+
+import java.util.ArrayList;
+
+import br.com.gimovel.model.Imovel;
+
+public class ImovelDao {
+
+	public void adicionar(){
+		
+	}
+	
+	public void remover(){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public ArrayList<Imovel> getImoveis(){
+		return null;
+	}
+	
+}
